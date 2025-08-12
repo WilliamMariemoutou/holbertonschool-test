1d4b243fa1101108048ea61895de59f78ec77765
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-08-12 11:00:58 +0400

    Swap two values

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - need to swap two values
+ * @a: first value
+ * @b: second value
+ *
+ */
+
+void swap_int(int *a, int *b)
+
+	int temp = 0;
+	{
+		temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+
+
