862b991ec8733c9cf1b865098715cb979ffe21e5
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-08-22 22:20:25 +0400

    Creates a new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner);
+{
+	dog_t *d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return NULL;
+
+	if (name != NULL)
+	{
+		d -> name = malloc(strlen(name) + 1);
+		if (d -> name == NULL)
+		{
+			free(d);
+			return NULL;
+		}
+		strcpy(d -> name, name);
+	}
+	else
+		d -> name = NULL;
+
+	if (owner != NULL)
+	{
+		d -> owner = malloc(strlen(owner) + 1);
+		if (d -> owner == NULL)
+		{
+			free(d -> name);
+			free(d);
+			return NULL;
+		}
+		strcpy(d -> owner, owner);
+	}
+	else
+		d -> owner = NULL;
+
+	d -> age = age;
+	return d;
+}
