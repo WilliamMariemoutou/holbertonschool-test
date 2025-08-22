20d6d4f331a4d7ec1a07ff06e32872a58c94f1a7
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-08-22 22:39:07 +0400

    Frees dogs

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 
 /**
-* free_dog - frees all dogs
-* @d: pointer to the struct dog
-*
-*/
+ * free_dog - frees a dog_t struct
+ * @d: pointer to the dog_t struct
+ */
 void free_dog(dog_t *d)
 {
-     if (d == NULL)
-         return;
+	if (d == NULL)
+		return;
 
-     if (d->name != NULL)
-         free(d->name);
+	if (d->name != NULL)
+		free(d->name);
 
-     if (d->owner != NULL)
-         free(d->owner);
+	if (d->owner != NULL)
+		free(d->owner);
 
-	 free(d);
+	free(d);
 }
