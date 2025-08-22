2558e0c7013976f1b9b4df139f9b20188a084711
WilliamMariemoutou <Williammariemoutou04@gmail.com>
2025-08-22 22:22:56 +0400

    Creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
 
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d = malloc(sizeof(dog_t));
 	if (d == NULL)
